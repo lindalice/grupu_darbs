0af225da5ee43de3ec3e6356c1c69a860e0008fe
lindalice <73908773+lindalice@users.noreply.github.com>
2023-02-17 14:22:42 +0200

    1
    1

---
+++ backend/controllers/Users.js
@@ -0,0 +1,95 @@
+import User from "../models/UserModel.js";
+import argon2 from "argon2";
+
+export const getUsers = async(req, res) =>{
+    try {
+        const response = await User.findAll({
+            attributes:['uuid','name','email','role']
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const getUserById = async(req, res) =>{
+    try {
+        const response = await User.findOne({
+            attributes:['uuid','name','email','role'],
+            where: {
+                uuid: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const createUser = async(req, res) =>{
+    const {name, email, password, confPassword, role} = req.body;
+    if(password !== confPassword) return res.status(400).json({msg: "'Parole' un 'Apstiprināt paroli' nesakrīt"});
+    const hashPassword = await argon2.hash(password);
+    try {
+        await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        });
+        res.status(201).json({msg: "Veiksmīga reģistrācija"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
+}
+
+export const updateUser = async(req, res) =>{
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "Lietotājs nav atrasts"});
+    const {name, email, password, confPassword, role} = req.body;
+    let hashPassword;
+    if(password === "" || password === null){
+        hashPassword = user.password
+    }else{
+        hashPassword = await argon2.hash(password);
+    }
+    if(password !== confPassword) return res.status(400).json({msg: "'Parole' un 'Apstiprināt paroli' nesakrīt"});
+    try {
+        await User.update({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        },{
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Lietotaja informacija atjaunota"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
+}
+
+export const deleteUser = async(req, res) =>{
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "ULietotājs nav atrasts"});
+    try {
+        await User.destroy({
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Lietotajs izdzests!"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
+}
